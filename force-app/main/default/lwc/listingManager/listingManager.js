6c65fbf3b949e7c22bb7468228ca6dce2fa6e3b0
Karan3036 <karsingh9377@gmail.com>
2026-09-24 17:57:39 +0530

    Uncomment Control Center Code

--- force-app/main/default/lwc/listingManager/listingManager.js
+++ force-app/main/default/lwc/listingManager/listingManager.js
@@ -2,10 +2,10 @@ import { LightningElement, track, api, wire } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import designcss from '@salesforce/resourceUrl/listingManagerCss';
 import getListingData from '@salesforce/apex/ListingManagerController.getListingData';
-// import getMetadataRecords from '@salesforce/apex/ControlCenterController.getMetadataRecords';
+import getMetadataRecords from '@salesforce/apex/ControlCenterController.getMetadataRecords';
 import { NavigationMixin } from 'lightning/navigation';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import LISTING_OBJECT from '@salesforce/schema/MVEX__Listing__c';
+import LISTING_OBJECT from '@salesforce/schema/Listing__c';
 import MulishFontCss from '@salesforce/resourceUrl/MulishFontCss';
 import { errorDebugger } from 'c/globalProperties';
 import USER_CURRENCY from '@salesforce/i18n/currency';
@@ -447,25 +447,23 @@ export default class ListingManager extends NavigationMixin(LightningElement) {
     }
 
     getAccessible() {
-        // getMetadataRecords()
-        //     .then(data => {
-        //         const listingManagerFeature = data.find(
-        //             item => item.DeveloperName === 'Listing_Manager'
-        //         );
-        //         this.isAccessible = listingManagerFeature ? Boolean(listingManagerFeature.MVEX__isAvailable__c) : false;
-        //         if (this.isAccessible) {
-        //             this.getListingDataMethod();
-        //         } else {
-        //             this.spinnerShow = false;
-        //         }
-        //     })
-        //     .catch(error => {
-        //         console.error('Error fetching accessible fields', error);
-        //         this.isAccessible = false;
-        //         this.spinnerShow = false;
-        //     });
-        this.isAccessible = true;
-        this.getListingDataMethod();
+        getMetadataRecords()
+            .then(data => {
+                const listingManagerFeature = data.find(
+                    item => item.DeveloperName === 'Listing_Manager'
+                );
+                this.isAccessible = listingManagerFeature ? Boolean(listingManagerFeature.MVEX__isAvailable__c) : false;
+                if (this.isAccessible) {
+                    this.getListingDataMethod();
+                } else {
+                    this.spinnerShow = false;
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching accessible fields', error);
+                this.isAccessible = false;
+                this.spinnerShow = false;
+            });
     }
 
     /**
